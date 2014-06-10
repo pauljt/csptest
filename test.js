387fd25ba84281ef12501db4967a72e3f5d6dfc1
Paul Theriault <pauljt@mozilla.com>
2014-06-10 21:25:40 +1000

    added basic tests, and example of dynamically linking a style

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var testDiv;
 window.addEventListener('load', function () {
   testDiv = document.querySelector('#test');
-  for (var i = 1; i <= 7; i++) {
+  for (var i = 1; i <= 9; i++) {
     document.querySelector('#test' + i).addEventListener('click', this['test' + i])
   }
 
@@ -17,23 +17,55 @@ function test3() {
   testDiv.setAttribute('style', 'background:red');
   testDiv.textContent='red';
 }
-function test4() {
+
+function test4(){
+  testDiv.removeAttribute('style');
+  testDiv.textContent="imported style makes this indianred"
+  var style = document.createElement('style');
+  style.appendChild(document.createTextNode('@import url(\'test.css\');'));
+  document.head.appendChild(style);
+}
+
+function test5() {
   testDiv.style = "background:yellow";
   testDiv.textContent='yellow';
 }
-function test5() {
+function test6() {
   testDiv.style.background = "purple";
   testDiv.textContent='purple';
 }
 
-function test6(){
+function test7(){
   testDiv.removeAttribute('style');
   testDiv.textContent='No background';
 }
 
-function test7(){
+function test8(){
   testDiv.style.setProperty('background','orange');
   testDiv.textContent='Orange';
+}
+
+
+
+
+
+function test9(){
+  var stylesheet = 'icon.css';
+  var template = document.createElement('div');
+
+  var link=document.createElement('link');
+  link.setAttribute('href',stylesheet);
+  link.setAttribute('type','text/css');
+  link.setAttribute('rel','StyleSheet');
+  template.appendChild(link);
+
+  var content=document.createElement('div');
+  content.setAttribute('id','icon')
+
+  content.textContent='testing dynamic style loading using link'
+  template.appendChild(content);
+  testDiv.appendChild(template);
+
 }
 
 
